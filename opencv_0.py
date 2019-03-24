90ea93e3f01a40ffd1e82794c62a803bec08b4eb
romaric2291 <romaric2291@gmail.com>
2019-03-24 11:35:39 +0100

    Ajout d'un script pour openCV et d'une image pour visualiser

---
+++ opencv_0.py
@@ -0,0 +1,11 @@
+import cv2
+
+def display_picture(name):
+    picture = cv2.imread(name)
+    picture = cv2.resize(picture, (picture.shape[1]/4,picture.shape[0]/4))
+    cv2.imshow("My picture", picture)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+if __name__ == "__main__":
+    display_picture("malta.jpg")
