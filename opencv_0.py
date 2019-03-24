03342696f4019388994b30758a459f5f61940a5d
romaric2291 <romaric2291@gmail.com>
2019-03-24 11:39:50 +0100

    Ajout de la fonctionnalité noir et blanc

--- opencv_0.py
+++ opencv_0.py
@@ -7,5 +7,13 @@ def display_picture(name):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
+def display_picture_bw(name):
+    picture = cv2.imread(name,0)
+    picture = cv2.resize(picture, (picture.shape[1]/4,picture.shape[0]/4))
+    cv2.imshow("My picture", picture)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()    
+    
 if __name__ == "__main__":
-    display_picture("malta.jpg")
+    #display_picture("malta.jpg")
+    display_picture_bw("malta.jpg")
